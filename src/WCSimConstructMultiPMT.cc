4e7565ef05df3fa70436849ed5d26595c00a7d20
Tom Feusels <tfeusels@neut20.triumf.ca>
2015-09-04 16:20:24 -0700

    merge fitqun tuning branch with very latest mPMT branch

--- src/WCSimConstructMultiPMT.cc
+++ src/WCSimConstructMultiPMT.cc
@@ -59,7 +59,6 @@ G4LogicalVolume* WCSimDetectorConstruction::ConstructMultiPMT(G4String PMTName, 
   
   G4cout << "Create mPMT" << G4endl;
   
-  
   //All components of the PMT are now contained in a single logical volume logicWCPMT.
   //Origin is on the blacksheet, faces positive z-direction.
 
